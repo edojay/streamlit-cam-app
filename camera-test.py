3bca5345d972e45341bf8f3eb8ae65990ed0c3a2
Edin <esk_08@hotmail.com>
2022-09-27 20:58:04 +0200

    save image to phone

--- camera-test.py
+++ camera-test.py
@@ -1,6 +1,8 @@
 import streamlit as st
+from PIL import Image
 
-picture = st.camera_input("Take a picture")
+picture = st.camera_input("eeeeey du e fetttt jobbi")
 
 if picture:
     st.image(picture)
+    Image.save(picture)
